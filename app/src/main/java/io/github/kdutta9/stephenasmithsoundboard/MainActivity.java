5b749bd689a82c4016173e04fe297ae75b677bb0
Kunal <kunaldutta1749@gmail.com>
2019-06-17 23:15:20 -0700

    added sounds

--- app/src/main/java/io/github/kdutta9/stephenasmithsoundboard/MainActivity.java
+++ app/src/main/java/io/github/kdutta9/stephenasmithsoundboard/MainActivity.java
@@ -16,6 +16,16 @@ public class MainActivity extends AppCompatActivity {
     MediaPlayer sound8;
     MediaPlayer sound9;
     MediaPlayer sound10;
+    MediaPlayer sound11;
+    MediaPlayer sound12;
+    MediaPlayer sound13;
+    MediaPlayer sound14;
+    MediaPlayer sound15;
+    MediaPlayer sound16;
+    MediaPlayer sound17;
+    MediaPlayer sound18;
+    MediaPlayer sound19;
+    MediaPlayer sound20;
 
 
     @Override
@@ -32,10 +42,20 @@ public class MainActivity extends AppCompatActivity {
         sound8 = MediaPlayer.create(this, R.raw.sound8);
         sound9 = MediaPlayer.create(this, R.raw.sound9);
         sound10 = MediaPlayer.create(this, R.raw.sound10);
+        sound11 = MediaPlayer.create(this, R.raw.sound11);
+        sound12 = MediaPlayer.create(this, R.raw.sound12);
+        sound13 = MediaPlayer.create(this, R.raw.sound13);
+        sound14 = MediaPlayer.create(this, R.raw.sound14);
+        sound15 = MediaPlayer.create(this, R.raw.sound15);
+        sound16 = MediaPlayer.create(this, R.raw.sound16);
+        sound17 = MediaPlayer.create(this, R.raw.sound17);
+        sound18 = MediaPlayer.create(this, R.raw.sound18);
+        sound19 = MediaPlayer.create(this, R.raw.sound19);
+        sound20 = MediaPlayer.create(this, R.raw.sound20);
     }
 
     public void sound(View view) {
-        double randomNum = Math.random() * 10 + 1;
+        double randomNum = Math.random() * 20 + 1;
         int selector = (int) randomNum;
         if (selector == 1) {
             sound1.start();
@@ -66,6 +86,36 @@ public class MainActivity extends AppCompatActivity {
         }
         if (selector == 10) {
             sound10.start();
+        }
+        if (selector == 11) {
+            sound11.start();
+        }
+        if (selector == 12) {
+            sound12.start();
+        }
+        if (selector == 13) {
+            sound13.start();
+        }
+        if (selector == 14) {
+            sound14.start();
+        }
+        if (selector == 15) {
+            sound15.start();
+        }
+        if (selector == 16) {
+            sound16.start();
+        }
+        if (selector == 17) {
+            sound17.start();
+        }
+        if (selector == 18) {
+            sound18.start();
+        }
+        if (selector == 19) {
+            sound19.start();
+        }
+        if (selector == 20) {
+            sound20.start();
         }
     }
 }
